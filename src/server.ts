ac0981e988059f26ac875e75d9ae72071874c37e
akash <a4076577@gmail.com>
2025-12-21 15:46:12 +0530

    increase json size

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,10 @@ const app: Application = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // Parse JSON bodies
+// app.use(express.json()); // Parse JSON bodies
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
+
 
 // Database Connection
 const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/uppsc_db';
